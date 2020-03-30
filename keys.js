e285e1de61fb44d0646cdff6d25f2c38a9d8fa50
cheebachong <atrodondi@gmail.com>
2020-03-30 16:29:35 -0700

    added command conditions for first argument and input of multiple word songs/movies/bands etc. need to write conert and movie api

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-console.log("this loaded");
+console.log("keys loaded successfully, mate!");
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
--- liri.js
+++ liri.js
@@ -1,26 +1,60 @@
+//read and set any environment variables with the dotenv package
 require("dotenv").config();
+
+// gets my spotify keys from keys.js and stores in Keys
 var keys = require("./keys.js");
+
+// getting access to node spotify api and sets to variable
 var Spotify = require("node-spotify-api");
+
+//Spotify object constructor
 function Spotify(artists, name, link, album) {
   this.artists = artists,
     this.name = name,
     this.link = link,
     this.album = album;
 }
+
+// creates the spotify object that can be searched
 var spotify = new Spotify(keys.spotify);
+
 var query = "";
+
+var input = process.argv[2];
+
+//query take in as many arg as user enters after command arg
 for (let i = 3; i < process.argv.length; i++) {
-  query += process.argv[i]
-
+  query += process.argv[i] + " ";
 }
-spotify.search({ type: "track", query: query, limit: 1 }, function (err, data) {
-  if (err) {
-    return console.log("error occurred: " + err);
+
+// command function that defines what input does based on command input (first argument in command line)
+
+function command() {
+  if (input == "spotify-this-song") {
+    spotify.search({ type: "track", query: query, limit: 1 }, function (err, data) {
+      if (err) {
+        return console.log("error occurred: " + err);
+      }
+      // album
+      console.log("Album: " + data.tracks.items[0].album.name);
+
+      // name of Track Searched
+      console.log("Track Name: " + data.tracks.items[0].name);
+
+      // artist of Track Searched
+      console.log("Artist: " + data.tracks.items[0].artists[0].name);
+
+      // preview URL to listen to 30 second preview (if avail)
+      console.log("Preview URL (if avail): " + data.tracks.items[0].preview_url);
+
+
+    });
   }
-  console.log(data.tracks.items[0].album.name);
-  console.log(data.tracks.items[0].name);
-  console.log(data.tracks.items[0].artists[0].name);
-  console.log(data.tracks.items[0].preview_url);
+  // concert command accessing Band In Town API
+  else if (input == "concert-this") { console.log("concert? Is " + query + "on the set list?") }
+  else if (input == "movie-this") { console.log(query + " is a great movie!") }
+  else { console.log("That is not a proper command. => 'spotify-this-song' / 'concert-this' / 'movie-this' ") }
+}
 
+command();
 
-});
