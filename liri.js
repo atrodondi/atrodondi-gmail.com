5c0c5ebdcf65ff2cf50adb0343ea9ce318b5e789
cheebachong <atrodondi@gmail.com>
2020-03-31 14:12:20 -0700

    changed layout so output to terminal would look nicer and being easier to read, wasnt necessary but whatever

--- liri.js
+++ liri.js
@@ -58,17 +58,10 @@ function command() {
         if (err) {
           return console.log("No Song Found! Check Song Title.");
         }
-        // album
-        console.log("Album: " + data.tracks.items[0].album.name);
+        let obj = data.tracks.items[0];
+        console.log("\n>>>>>>>>>>>>>>>>>>>>>>>>>", "\nAlbum: " + obj.album.name, "\nTrack Name: " + obj.name, "\nArtist: " + obj.artists[0].name, "\nPreview URL (if avail): " + obj.preview_url, '\n>>>>>>>>>>>>>>>>>>>>>>>>>', "\n");
 
-        // name of Track Searched
-        console.log("Track Name: " + data.tracks.items[0].name);
 
-        // artist of Track Searched
-        console.log("Artist: " + data.tracks.items[0].artists[0].name);
-
-        // preview URL to listen to 30 second preview (if avail)
-        console.log("Preview URL (if avail): " + data.tracks.items[0].preview_url);
 
 
       });
@@ -78,7 +71,7 @@ function command() {
   // concert command accessing Band In Town API and gathering: name of the venue,venue location, date of the event "MM/DD/YYYY" format
   else if (input == "concert-this") {
     //query takes in as many arg as user enters after command arg
-    var query = "";
+    let query = "";
     for (let i = 3; i < process.argv.length; i++) {
       query += process.argv[i];
     }
@@ -90,7 +83,7 @@ function command() {
       axios.get("https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp").then(function (response) {
         for (i in response.data) {
           let obj = response.data[i];
-          console.log("\n\nVenue: " + obj.venue.name, "\nLocation: " + obj.venue.city + ", " + obj.venue.country, "\nDate: " + moment(obj.datetime).format("MM/DD/YYYY"));
+          console.log("\n\n>>>>>>>>>>>>>>>>>>>>>>>>>", "\nVenue: " + obj.venue.name, "\nLocation: " + obj.venue.city + ", " + obj.venue.country, "\nDate: " + moment(obj.datetime).format("MM/DD/YYYY"), "\n>>>>>>>>>>>>>>>>>>>>>>>>>", "\n");
 
         }
       });
@@ -113,15 +106,9 @@ function command() {
       axios.get("https://www.omdbapi.com/?t=Mr.+nobody&y=&plot=short&apikey=trilogy").then(function (response) {
 
 
+        let obj = response.data;
 
-        console.log("Movie Title: " + response.data.Title);
-        console.log("Year: " + response.data.Year);
-        console.log("This movie's IMDB rating is: " + response.data.imdbRating);
-        console.log("This movie's Rotten Tomates rating is: " + response.data.Ratings[1].Value);
-        console.log("This movie was produced in: " + response.data.Country);
-        console.log("Language: " + response.data.Language);
-        console.log("Plot: " + response.data.Plot);
-        console.log("Actors: " + response.data.Actors);
+        console.log("\n\n>>>>>>>>>>>>>>>>>>>>>>>>>", "\nMovie Title: " + response.data.Title, "\nYear: " + obj.Year, "\nIMDB rating: " + obj.imdbRating, "\nRotten Tomatoes rating: " + obj.Ratings[1].Value, "\n Country of Origin: " + obj.Country, "\nLanguage: + " + obj.Language, "\nPlot: " + obj.Plot, "\nCast: " + obj.Actors, "\n>>>>>>>>>>>>>>>>>>>>>>>>>", "\n\n");
 
       })
     }
@@ -130,16 +117,10 @@ function command() {
 
       axios.get("http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy").then(
         function (response) {
+          let obj = response.data;
 
+          console.log("\n\n>>>>>>>>>>>>>>>>>>>>>>>>>", "\nMovie Title: " + response.data.Title, "\nYear: " + obj.Year, "\nIMDB rating: " + obj.imdbRating, "\nRotten Tomatoes rating: " + obj.Ratings[1].Value, "\n Country of Origin: " + obj.Country, "\nLanguage: + " + obj.Language, "\nPlot: " + obj.Plot, "\nCast: " + obj.Actors, "\n>>>>>>>>>>>>>>>>>>>>>>>>>", "\n\n");
 
-          console.log("Movie Title: " + response.data.Title);
-          console.log("Year: " + response.data.Year);
-          console.log("This movie's IMDB rating is: " + response.data.imdbRating);
-          console.log("This movie's Rotten Tomates rating is: " + response.data.Ratings[1].Value);
-          console.log("This movie was produced in: " + response.data.Country);
-          console.log("Language: " + response.data.Language);
-          console.log("Plot: " + response.data.Plot);
-          console.log("Actors: " + response.data.Actors)
 
         }
       );
