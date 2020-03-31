22cbabe923b4594d243356aa4e394187b747d1f5
cheebachong <atrodondi@gmail.com>
2020-03-31 13:58:14 -0700

    got all requests working, need to handle errors or if user mispells band/movie etc. sometimes band api wont give answers, but will just reprompt command line

--- liri.js
+++ liri.js
@@ -15,17 +15,12 @@ var spotify = new Spotify(keys.spotify);
 
 // axios package
 var axios = require("axios");
-// omdb package...am i going to use? not sure yet.
-var omdb = require('omdb');
 
+// moment.js
+var moment = require("moment");
 
+// command input
 var input = process.argv[2];
-
-//query takes in as many arg as user enters after command arg
-var query = "";
-for (let i = 3; i < process.argv.length; i++) {
-  query += process.argv[i] + " ";
-}
 
 
 
@@ -34,6 +29,10 @@ for (let i = 3; i < process.argv.length; i++) {
 function command() {
   // if spotify command is entered, then access spotify api and get album, title of track searched, artist of track, and a Preview Url for a 30secon preview if there is one
   if (input == "spotify-this-song") {
+    var query = "";
+    for (let i = 3; i < process.argv.length; i++) {
+      query += process.argv[i] + " ";
+    }
     //if song title is blank, defaults to The Sign by Ace of Base
     if (query == "") {
       spotify.search({ type: "track", query: "The Sign", limit: 1 }, function (err, data) {
@@ -78,38 +77,52 @@ function command() {
   }
   // concert command accessing Band In Town API and gathering: name of the venue,venue location, date of the event "MM/DD/YYYY" format
   else if (input == "concert-this") {
-    console.log("concert? Is " + query + "on the set list?");
-    if ("") {
-      console.log("Band/Artist argument is blank!")
+    //query takes in as many arg as user enters after command arg
+    var query = "";
+    for (let i = 3; i < process.argv.length; i++) {
+      query += process.argv[i];
+    }
+
+    if (query == "") {
+      console.log("Type in the name of a Band or Artist !")
     }
     else {
-      axios.get("https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp");
-      console.log(response);
+      axios.get("https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp").then(function (response) {
+        for (i in response.data) {
+          let obj = response.data[i];
+          console.log("\n\nVenue: " + obj.venue.name, "\nLocation: " + obj.venue.city + ", " + obj.venue.country, "\nDate: " + moment(obj.datetime).format("MM/DD/YYYY"));
+
+        }
+      });
 
     }
 
   }
 
-  // movie command accessing omdb api getting: movie title, year it came out, IMDB rating, rotten tomatoes rating, country where produced, language, plot, and actors in movie.
+  // had to put a separate query definer for movie because he needs to have a space between each arguement, where as Band/Spotify did not need spaces added. kept getting errors when trying to access band api with spaces in bewteen arguments
   else if (input == "movie-this") {
+    let query = "";
+    for (let i = 3; i < process.argv.length; i++) {
+      query += process.argv[i] + " ";
+    }
+
 
 
     // if query is blank, sets default to "Mr. Nobody" per instructions
     if (query == "") {
-      axios.get("http://www.omdbapi.com/?t=Mr.+nobody&y=&plot=short&apikey=trilogy").then(function (err, response) {
-        if (err) {
-          return console.log("There was an error: " + err)
-        }
-        else {
-          console.log("Movie Title: " + response.data.title);
-          console.log("Year: " + response.data.year);
-          console.log("This movie's IMDB rating is: " + response.data.imdbRating);
-          console.log("This movie's Rotten Tomates rating is: " + response.data.Ratings[1].Value);
-          console.log("This movie was produced in: " + response.data.Country);
-          console.log("Language: " + response.data.Language);
-          console.log("Plot: " + response.data.Plot);
-          console.log("Actors: " + response.data.Actors);
-        }
+      axios.get("https://www.omdbapi.com/?t=Mr.+nobody&y=&plot=short&apikey=trilogy").then(function (response) {
+
+
+
+        console.log("Movie Title: " + response.data.Title);
+        console.log("Year: " + response.data.Year);
+        console.log("This movie's IMDB rating is: " + response.data.imdbRating);
+        console.log("This movie's Rotten Tomates rating is: " + response.data.Ratings[1].Value);
+        console.log("This movie was produced in: " + response.data.Country);
+        console.log("Language: " + response.data.Language);
+        console.log("Plot: " + response.data.Plot);
+        console.log("Actors: " + response.data.Actors);
+
       })
     }
     // filled out movie query 
